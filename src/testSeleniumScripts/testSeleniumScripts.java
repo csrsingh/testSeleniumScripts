43a3a6a617d880ec7fcedbae0e7a6d50dd0be449
Chandrashekhar Singh <cssingh.sv@gmail.com>
2023-03-20 15:34:11 +0530

    testAlertPopup and test xpath

--- src/testSeleniumScripts/testSeleniumScripts.java
+++ src/testSeleniumScripts/testSeleniumScripts.java
@@ -7,9 +7,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class testSeleniumScripts {
 
 	public static void main(String[] args) {
-		WebDriver drv = new ChromeDriver();
-		//WebDriver drv = new FirefoxDriver();
-		drv.get("www.google.com");
+		//WebDriver drv = new ChromeDriver();
+		WebDriver drv = new FirefoxDriver();
+		drv.get("https://testingfundamental.com/");
 
 	}
 
